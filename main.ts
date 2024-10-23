f90ddc47e06875b4bf9c664ed617ac768f056a79
gavin-gallant <gavin.gallant@mths.ca>
2024-10-23 10:48:52 -0400

    made code works no bugs

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: Oct 2024
+ * This program uses a servo motor
 */
 
-basic.showString('Hello, World!')
+
+// variables
+const servoNumber1 = robotbit.Servos.S1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 30)
+    basic.clearScreen()
+    basic.showString('30')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
